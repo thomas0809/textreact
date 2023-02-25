028a5cde426e36f50526f873ce1bd8def2e01257
uranium11010 <zed.zhening.li@hotmail.com>
2023-02-25 18:42:10 -0500

    Update README

--- data/convert_pistachio.py
+++ data/convert_pistachio.py
@@ -10,7 +10,7 @@ from rdkit import RDLogger
 parser = argparse.ArgumentParser()
 parser.add_argument("--rtype", required=True)
 parser.add_argument("--split", choices=["trn", "tst"], required=True)
-parser.add_argument("--years", type=int, action="append")
+parser.add_argument('-y', "--years", type=int, action="append")
 parser.add_argument("--clist")
 
 args = parser.parse_args()
@@ -19,52 +19,6 @@ RDLogger.DisableLog('rdApp.*')
 
 data_dirs = [f"../pistachio/extract/grants/{i}" for i in args.years]  # years 2000 to 2021
 start = time.time()
-
-# data = defaultdict(list)
-# condition_dict = {}
-# num_canon_err = 0
-# i = 0
-# for data_dir in data_dirs:
-#     print("Loading:", data_dir)
-#     for file_name in os.listdir(data_dir):
-#         path = os.path.join(data_dir, file_name)
-#         if os.path.isfile(path) and file_name[0] != '.':
-#             with open(path, 'r') as f:
-#                 data_lines = f.readlines()
-#                 for line in data_lines:
-#                     item = json.loads(line)
-#                     reaction = item["data"]["smiles"]
-#                     i = reaction.index('>') + 1  # start of conditions
-#                     j = reaction[i:].index('>') + i  # second >
-#                     k = reaction.index(' ') if ' ' in reaction else len(reaction)
-#                     reaction_smiles = reaction[:i] + reaction[j:k]
-#                     conditions = []
-#                     for component in item["components"]:
-#                         if component["role"] in ["Solvent", "Agent", "Catalyst"] and "smiles" in component:
-#                             try:
-#                                 canon_smiles = Chem.CanonSmiles(component["smiles"])
-#                             except:
-#                                 canon_smiles = component["smiles"]
-#                                 # print("BAD COND:", component["smiles"])
-#                                 num_canon_err += 1
-#                             if canon_smiles not in condition_dict:
-#                                 condition_dict[canon_smiles] = len(condition_dict)
-#                             conditions.append(condition_dict[canon_smiles])
-#                     if conditions not in data[reaction_smiles]:
-#                         data[reaction_smiles].append(conditions)
-#         # break
-#         i += 1
-# print("NUM FILES:", i)
-# print("NUM REACTIONS:", len(data))
-# print("NUM CANON ERR:", num_canon_err)
-# print("NUM CONDITIONS:", len(condition_dict))
-# print("TIME:", time.time() - start)
-# 
-# condition_list = [None for _ in range(len(condition_dict))]
-# for k, v in condition_dict.items():
-#     condition_list[v] = k
-# 
-# np.savez("data_example.npz", data=np.array([dict(data), condition_list], dtype='O'))
 
 i = 0
 num_err = 0
